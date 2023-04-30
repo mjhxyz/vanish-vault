339ceb8c57a56af64d13bcf78b1e39db9798feee
Mao <mjhxyz@foxmail.com>
2023-05-01 00:42:44 +0800

    update: add MANIFEST.in

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/mjhxyz/vanish-vault',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Flask>=2.2.3',
         'PyCryptodome>=3.10',
