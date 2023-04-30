215f6a4918649ff7a86c901b3e0be0d34e8af807
Mao <mjhxyz@foxmail.com>
2023-04-30 15:49:44 +0800

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vanishvault',
+    version='0.0.1',
+    author='mjhxyz',
+    author_email='mjhxyz@foxmail.com',
+    description='Flask-built, open-source project like Pastebin. Upload encrypted, self-deleting short messages. Emphasizes message security and ephemerality, suitable for sensitive information transmission without leaving traces.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mjhxyz/vanish-vault',
+    packages=find_packages(),
+    install_requires=[
+        'Flask>=2.2.3',
+        'PyCryptodome>=3.10',
+    ],
+    entry_points={
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
