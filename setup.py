598f322fca8989bc499956391626eda6235bd508
mjhxyz <mjxhyz@foxmail.com>
2023-05-01 10:29:18 +0800

    update: fix different color for textarea

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='mjhxyz',
     author_email='mjhxyz@foxmail.com',
     description='Flask-built, open-source project like Pastebin. Upload encrypted, self-deleting short messages. Emphasizes message security and ephemerality, suitable for sensitive information transmission without leaving traces.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/mjhxyz/vanish-vault',
     packages=find_packages(),
