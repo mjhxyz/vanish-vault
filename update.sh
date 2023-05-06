e8b4fdfd72f28c37a0a564ef893df68bdd15a639
mjhxyz <mjxhyz@foxmail.com>
2023-05-06 11:48:32 +0800

    update: update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,6 @@
 PROJECT_SRC="/home/mao/git/vv"
 PROJECT_PATH="/home/mao/project/vv"
 
-cd $PROJECT_SRC
-git stash
-git pull origin master
-git stash pop
-
 source $PROJECT_PATH/venv/bin/activate
+cd $PROJECT_SRC
 pip install .
